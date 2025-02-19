767e9450e4809a70d1f62bf4b383b2affe8e4a11
Olivia Dondas <olivia.dondas@laplateforme.io>
2025-02-19 10:44:22 +0100

    maj bdd

--- pages/commentaire.php
+++ pages/commentaire.php
@@ -1,5 +1,29 @@
-Un formulaire d’ajout de commentaire (commentaire.php) : ce
+
+
+<?php
+require_once __DIR__ . '/../models/Database.php';
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Livre d'or</title>
+    <link rel="stylesheet" href="../css/global.css">
+</head>
+<body>
+    <p> Un formulaire d’ajout de commentaire (commentaire.php) : ce
 formulaire ne contient qu’un champ permettant de rentrer son
 commentaire et un bouton de validation. Il n’est accessible qu’aux
 utilisateurs connectés. Chaque utilisateur peut poster plusieurs
-commentaires.
+commentaires. </p>
+
+    <a href="profil.php">Profil</a>
+    <a href="commentaire.php">Commentaire</a>
+    <a href="register.php">Inscription</a>
+    <a href="livre-or.php">Livre d'or</a>
+</body>
+</html>
